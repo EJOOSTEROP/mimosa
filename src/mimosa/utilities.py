2c0bf259e69bb009009327b04143eb6e9191a225
Erik Oosterop <erik.oosterop.khesw@aleeas.com>
2023-09-21 00:12:41 +0000

    Add chunker.

--- src/mimosa/utilities.py
+++ src/mimosa/utilities.py
@@ -1,9 +1,9 @@
 """Various random snippets."""
 from datetime import date, timedelta
-from typing import Iterator
+from typing import Iterator, Optional
 
 
-def daterange(start_date: date, end_date: date) -> Iterator[date]:
+def daterange(start_date: Optional[date] = None, end_date: Optional[date] = None) -> Iterator[date]:
     """Generates a range of dates.
 
     Args:
@@ -16,16 +16,34 @@ def daterange(start_date: date, end_date: date) -> Iterator[date]:
     >>> import datetime
     >>> start_date = datetime.datetime.strptime("2023-08-10", "%Y-%m-%d").astimezone()
     >>> end_date = datetime.datetime.strptime("2023-08-15", "%Y-%m-%d").astimezone()
-    >>> print([dte.strftime("%Y-%m-%d") for dte in daterange(start_date, end_date)])
-    ['2023-08-10', '2023-08-11', '2023-08-12', '2023-08-13', '2023-08-14']
-    >>> print([dte.strftime("%Y-%m-%d") for dte in daterange(end_date, start_date)])
-    ['2023-08-10', '2023-08-11', '2023-08-12', '2023-08-13', '2023-08-14']
-    >>> print([dte.strftime("%Y-%m-%d") for dte in daterange(start_date, None)])
-    ['2023-08-10']
-    >>> print([dte.strftime("%Y-%m-%d") for dte in daterange(None, end_date)])
-    ['2023-08-14']
-    >>> print([dte.strftime("%Y-%m-%d") for dte in daterange(None, None)])
-    []
+
+    >>> for dte in daterange(start_date, end_date):
+    ...     print(dte.strftime("%Y-%m-%d"))
+    2023-08-10
+    2023-08-11
+    2023-08-12
+    2023-08-13
+    2023-08-14
+
+    >>> for dte in daterange(end_date, start_date):
+    ...     print(dte.strftime("%Y-%m-%d"))
+    2023-08-10
+    2023-08-11
+    2023-08-12
+    2023-08-13
+    2023-08-14
+
+    >>> for dte in daterange(start_date):
+    ...     print(dte.strftime("%Y-%m-%d"))
+    2023-08-10
+
+    >>> for dte in daterange(end_date = end_date):
+    ...     print(dte.strftime("%Y-%m-%d"))
+    2023-08-14
+
+    >>> for dte in daterange():
+    ...     print(dte.strftime("%Y-%m-%d"))
+
     >>> print([dte.strftime("%Y-%m-%d") for dte in daterange(start_date, start_date)])
     []
     """
@@ -37,11 +55,30 @@ def daterange(start_date: date, end_date: date) -> Iterator[date]:
     elif start_date is None:
         start_date = end_date - timedelta(days=1)
 
-    if start_date > end_date:  # reversed dates work. Is this desired?
+    if start_date > end_date:  # reversed dates are accepted. Is this desired?
         start_date, end_date = end_date, start_date
 
     for n in range(int((end_date - start_date).days)):
         yield start_date + timedelta(n)
+
+
+def chunker(seq: list, size: int) -> Iterator[list]:
+    """Generate an iterator that yields chunks of the sequence with the specified size.
+
+    Args:
+        seq (list): The sequence to be chunked.
+        size (int): The size of each chunk.
+
+    Yields:
+        Iterator[list]: A generator that yields lists of size `size` from `seq`.
+
+    >>> for chunk in chunker([1, 2, 3, 4, 5], 2):
+    ...     print(chunk)
+    [1, 2]
+    [3, 4]
+    [5]
+    """
+    return (seq[pos : pos + size] for pos in range(0, len(seq), size))
 
 
 class MissingEnvironmentVariableError(Exception):
