b98361d82a2ebb4dbb22a765ec9f47409c9a0335
Erik Oosterop <erik.oosterop.khesw@aleeas.com>
2023-10-15 01:44:15 +0000

    Run for missing data.

--- src/mimosa/wip.py
+++ src/mimosa/wip.py
@@ -14,8 +14,8 @@ reporting_update = False
 run_this = True
 if run_this:
     pipeline.run_landing_pipeline(
-        gas_date=date(2023, 9, 29),  # still from 2019-01-01 to 2019-09-01
-        to_gas_date=date(2023, 9, 30),
+        gas_date=date(2023, 9, 27),  # still from 2019-01-01 to 2019-09-01
+        to_gas_date=date(2023, 10, 1),
         reporting_update=reporting_update,
     )
 else:
