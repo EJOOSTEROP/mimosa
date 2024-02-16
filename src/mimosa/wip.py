e63243e2682b0b6d181c549dce887395c97f0230
Erik Oosterop <erik.oosterop.khesw@aleeas.com>
2024-02-16 19:31:17 +0000

    wip changes

--- src/mimosa/wip.py
+++ src/mimosa/wip.py
@@ -4,19 +4,20 @@ Loads data from European Gas data REST API.
 
 Loads data incrementally into DuckDB.
 """
+
 from datetime import date  # F401
 
 from mimosa.pipelines import GEI
 
-destination = "filesystem"  # "filesystem" "motherduck"
+destination = "motherduck"  # "filesystem" "motherduck"
 pipeline = GEI(destination=destination)
 reporting_update = False
 
 run_this = True
 if run_this:
     pipeline.run_landing_pipeline(
-        gas_date=date(2023, 9, 27),  # still from 2019-01-01 to 2019-09-01
-        to_gas_date=date(2023, 9, 28),
+        gas_date=date(2024, 2, 13),  # still from 2019-01-01 to 2019-09-01
+        to_gas_date=date(2024, 2, 14),
         reporting_update=reporting_update,
     )
     """
